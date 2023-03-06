7ef200a8e4262709d3d2b73e383fe340b865a01b
Logan Jones <loganjones9117@gmail.com>
2023-03-05 21:15:11 -0700

    feat: encoding table for tokenization

--- rustgpt/src/main.rs
+++ rustgpt/src/main.rs
@@ -1,38 +1,33 @@
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 use std::fs::File;
 use std::io::prelude::*;
+use std::collections::HashMap;
 
 fn main() {
 
-    // Open file     
+    // Open file 
     let mut file = File::open("test.txt").expect("Can't open file!");
 
     // Get file contents 
     let mut contents = String::new();
     file.read_to_string(&mut contents).expect("File contents can't be read into string");
 
-    // Print file contents 
-    println!("File contents: {}", contents);
+    // Get vocab: defines the set of possible characters that 
+    // the model can generate.
+    let mut vocab:Vec<char> = contents.chars().collect();
+    vocab.sort();
+    vocab.dedup();
+
+
+    // Tokenize: convert characters into a sequence of integers.
+    // Define encoding table: 
+    let mut stoi = HashMap::new();
+    let mut itos = HashMap::new();
+
+    for (i, ch) in vocab.iter().enumerate() {
+        stoi.insert(*ch, i);
+        itos.insert(i, *ch);
+    }
+} 
 
 
 
-    // Get set of unique chars from contents
-    let mut chars:Vec<char> = contents.chars().collect();
-    chars.sort();
-    chars.dedup();
-
-    let len = chars.len();
-}   
